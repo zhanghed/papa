d69f19db004f89ece9fe88b527022104cdee774f
zhanghed <747988598@qq.com>
2023-04-01 10:59:42 +0800

    w

---
+++ biying_wallpaper.py
@@ -0,0 +1,23 @@
+# 获取必应今日壁纸
+import requests
+import time
+
+url1 = "https://cn.bing.com/HPImageArchive.aspx"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1111111111; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0",
+}
+data = {
+    'format': 'js',
+    'idx': '1',
+    'n': '999',
+    'nc': int(round((time.time()) * 1000)),
+    'pid': 'hp',
+}
+req = requests.get(url=url1, headers=headers, params=data)
+req = req.json()
+for i in req["images"]:
+    name = i['title']
+    url2 = "https://cn.bing.com" + i['url']
+    req = requests.get(url=url2, headers=headers)
+    with open(name + '.jpg', 'wb') as f:
+        f.write(req.content)
---
+++ douban_top250.py
@@ -0,0 +1,32 @@
+import requests, re
+
+url = "https://movie.douban.com/j/chart/top_list"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/111.0.0.0 Safari/537.36",
+}
+data = {
+    "type": "13",
+    "interval_id": "100:90",
+    "action": "",
+    "start": "0",
+    "limit": "20"
+}
+
+if __name__ == '__main__':
+    try:
+        req = requests.get(url=url,headers=headers,params=data)
+        # req = requests.get(url=url,headers=headers,data=data)
+
+        req.encoding = "utf-8"
+        print(req)
+
+        # print(req.text)
+        # print(req.json())
+
+        rr = re.compile(r'"title":"(?P<title>.*?)","url":"(?P<url>.*?)","release_date":')
+        for i in rr.finditer(req.text):
+            print(i.group("title"), i.group("url"))
+
+    except Exception as e:
+        print(e)
