4dcf4bb552c857f8a58d6261e1e7fde2e08d689e
zhanghed <747988598@qq.com>
2023-04-12 16:36:04 +0800

    h

---
+++ a.py
@@ -0,0 +1,13 @@
+import os
+
+# https://pypi.douban.com/simple # 豆瓣镜像
+
+# https://pypi.tuna.tsinghua.edu.cn/simple # 清华镜像
+
+mirror = " -i https://pypi.douban.com/simple"
+
+os.system("python -m pip install --upgrade pip" + mirror)  # 更新 pip
+
+#os.system(r"pip install --target=路径 包名" + mirror)  # 安装
+
+os.system("pip install aiohttp" + mirror)  # 安装
