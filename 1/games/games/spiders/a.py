047458c0379606d795b606e3ae904430471f5d84
zhanghed <747988598@qq.com>
2023-04-27 11:15:21 +0800

    w

---
+++ 1/games/games/spiders/a.py
@@ -0,0 +1,17 @@
+import scrapy
+
+
+class ASpider(scrapy.Spider):
+    name = "a"
+    allowed_domains = ["4399.com"]
+    start_urls = ["https://www.4399.com/flash/"]
+
+    def parse(self, response):
+        r = response.xpath("/html/body/div[8]/ul/li")
+        for i in r:
+            a = i.xpath("./a/@href").extract_first()
+            b = i.xpath("./a/b/text()").extract_first()
+            data = {
+                "name": b,
+                "url": a
+            }
